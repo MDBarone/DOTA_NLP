95bbc9378aca453155c8cb8489538d0137c718cb
mmustaine <baronemda@gmail.com>
2018-07-11 19:38:43 -0700

    Added SQL upload functions.  Next will refactor create table code and then begin NLP analysis

--- importDotaBase.py
+++ importDotaBase.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
-import glob, csv, ast
+import glob, csv, ast, sys
+import mysql.connector
 
 class arayi(object):
+
+    def dbConnect(self):
+        cnx = mysql.connector.connect(user='root', password='root')
+        return cnx
+
+    ### EXTRACTS DB INFORMATION FOR IMPORT INTO SQL
     def getDBInfo(self,path):
         headerType=[]
-        print(path)
         # Is there a better way to do this line?
         tableName=path.replace("data/","").split(".csv")[0]
         with open(path,"r") as f:
@@ -19,13 +25,69 @@ class arayi(object):
                     headerType.append(type(head))
         return [tableName, header, headerType]
 
+    ### CREATE DATABASE AND TABLE SCHEMA
+    def initDB(self,dbName):
+        dB = dota.dbConnect()
+        print ("INITIALIZE DB")
+        self.cur = dB.cursor()
+        self.cur.execute("CREATE DATABASE %s;" % dbName)
+        self.cur.close()
+
+
+
+    def deleteDB(self,dbName):
+        dB = dota.dbConnect()
+        print("DROP DB")
+        self.cur = dB.cursor()
+        self.cur.execute("DROP DATABASE %s;" % dbName)
+
+    def createTable(self,schemaList):
+        # PANDAS SOLUTION.  REFACTOR CODE
+        # from sqlalchemy import create_engine
+        # import pandas as pd
+        #
+        # df = pd.read_csv('/PATH/TO/FILE.csv', sep='|')
+        # # Optional, set your indexes to get Primary Keys
+        # df = df.set_index(['COL A', 'COL B'])
+        #
+        # engine = create_engine('mysql://user:pass@host/db', echo=False)
+
+        df.to_sql(table_name, engine, index=False)
+        ### SCHEMA LIST IS
+        for table in schemaList:
+            header,variables,types=dota.getDBInfo(table)
+            varSchema=""
+            pk=variables[0]
+            for idx, value in enumerate(variables):
+                if value == "key":
+                    value="theKey"
+                if types[idx] == str:
+                    varSchema+=", \n %s %s" % (value, "VARCHAR(255)")
+                else:
+                    varSchema+=", \n %s %s" % (value, "FLOAT")
+
+            createTable="CREATE TABLE DOTA.%s (%s);" % (header, varSchema.lstrip(','))
+            print(createTable)
+            dB = dota.dbConnect()
+            self.cur = dB.cursor()
+            self.cur.execute(createTable)
+            print("Create table: %s" % header)
+        pass
+
+
+
 dota = arayi()
-dotabaseList=glob.glob("data/*.csv")
-for table in dotabaseList:
-    print(dota.getDBInfo(table))
+try:
+    dota.initDB("DOTA")
 
-###CREATE DB
+    ### GETS LIST OF TABLES FOR IMPORT
+    dotabaseList=glob.glob("data/*.csv")
+    # print(dotabaseList)
+    dota.createTable(dotabaseList)
 
 
-
-### FIND CSVS, MAKE TABLE WITH COLUMNS AND IMPORT
+    print("Success")
+    dota.deleteDB("DOTA")
+except EnvironmentError as e:
+    dota.deleteDB("DOTA")
+    print("Error %s:", e)
